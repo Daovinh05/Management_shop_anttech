5b0bb87fcf7c42f86628c0430afff5ac866cfa01
Congvinh2005 <vanxuank69@gmail.com>
2026-02-04 23:38:24 +0700

    Thanh toán vnpay v2

--- MVC/Core/VnPayHelper.php
+++ MVC/Core/VnPayHelper.php
@@ -1,68 +1,112 @@
 <?php
 require_once $_SERVER['DOCUMENT_ROOT'] . '/Banhang/MVC/Core/vnpay_config.php';
 
+// class VnPayHelper
+// {
+//     public static function createPaymentUrl($orderInfo, $amount, $orderId, $bankCode = null, $language = 'vn')
+//     {
+//         // Check if configuration is properly set
+//         if (VNPAY_TMNCODE === 'YOUR_VNP_TMNCODE' || VNPAY_HASHSECRET === 'YOUR_VNP_HASHSECRET') {
+//             throw new Exception("VNPAY configuration error: Please update your TMN_CODE and HASH_SECRET in vnpay_config.php");
+//         }
+
+//         $vnp_Url = VNPAY_URL;
+//         $vnp_Returnurl = VNPAY_RETURN_URL;
+//         $vnp_TmnCode = VNPAY_TMNCODE;
+//         $vnp_HashSecret = VNPAY_HASHSECRET;
+
+//         $vnp_TxnRef = $orderId; // Mã đơn hàng
+//         $vnp_OrderInfo = $orderInfo;
+//         $vnp_OrderType = 'billpayment';
+//         $vnp_Amount = $amount * 100; // Số tiền cần nhân với 100 để đúng định dạng
+//         $vnp_Locale = $language;
+//         $vnp_IpAddr = $_SERVER['REMOTE_ADDR'];
+
+//         $inputData = array(
+//             "vnp_Version" => "2.1.0",
+//             "vnp_TmnCode" => $vnp_TmnCode,
+//             "vnp_Amount" => $vnp_Amount,
+//             "vnp_Command" => "pay",
+//             "vnp_CreateDate" => date('YmdHis'),
+//             "vnp_CurrCode" => "VND",
+//             "vnp_IpAddr" => $vnp_IpAddr,
+//             "vnp_Locale" => $vnp_Locale,
+//             "vnp_OrderInfo" => $vnp_OrderInfo,
+//             "vnp_OrderType" => $vnp_OrderType,
+//             "vnp_ReturnUrl" => $vnp_Returnurl,
+//             "vnp_TxnRef" => $vnp_TxnRef,
+//         );
+
+//         if (isset($bankCode) && $bankCode != "") {
+//             $inputData['vnp_BankCode'] = $bankCode;
+//         }
+
+//         ksort($inputData);
+//         $query = "";
+//         $i = 0;
+//         $hashdata = "";
+//         foreach ($inputData as $key => $value) {
+//             if ($i == 1) {
+//                 $hashdata .= '&' . urlencode($key) . "=" . urlencode($value);
+//             } else {
+//                 $hashdata .= urlencode($key) . "=" . urlencode($value);
+//                 $i = 1;
+//             }
+//             $query .= urlencode($key) . "=" . urlencode($value) . '&';
+//         }
+
+//         $vnp_Url = $vnp_Url . "?" . $query;
+//         if (isset($vnp_HashSecret)) {
+//             $vnpSecureHash = hash_hmac('sha512', $hashdata, $vnp_HashSecret);
+//             $vnp_Url .= 'vnp_SecureHash=' . $vnpSecureHash;
+//         }
+
+//         return $vnp_Url;
+//     }
+
 class VnPayHelper
 {
-    public static function createPaymentUrl($orderInfo, $amount, $orderId, $bankCode = null, $language = 'vn')
+    public static function createPaymentUrl($orderInfo, $amount, $orderId, $language = 'vn')
     {
-        // Check if configuration is properly set
-        if (VNPAY_TMNCODE === 'YOUR_VNP_TMNCODE' || VNPAY_HASHSECRET === 'YOUR_VNP_HASHSECRET') {
-            throw new Exception("VNPAY configuration error: Please update your TMN_CODE and HASH_SECRET in vnpay_config.php");
-        }
-
         $vnp_Url = VNPAY_URL;
         $vnp_Returnurl = VNPAY_RETURN_URL;
         $vnp_TmnCode = VNPAY_TMNCODE;
         $vnp_HashSecret = VNPAY_HASHSECRET;
 
-        $vnp_TxnRef = $orderId; // Mã đơn hàng
-        $vnp_OrderInfo = $orderInfo;
-        $vnp_OrderType = 'billpayment';
-        $vnp_Amount = $amount * 100; // Số tiền cần nhân với 100 để đúng định dạng
-        $vnp_Locale = $language;
-        $vnp_IpAddr = $_SERVER['REMOTE_ADDR'];
-
         $inputData = array(
             "vnp_Version" => "2.1.0",
             "vnp_TmnCode" => $vnp_TmnCode,
-            "vnp_Amount" => $vnp_Amount,
+            "vnp_Amount" => $amount * 100,
             "vnp_Command" => "pay",
             "vnp_CreateDate" => date('YmdHis'),
             "vnp_CurrCode" => "VND",
-            "vnp_IpAddr" => $vnp_IpAddr,
-            "vnp_Locale" => $vnp_Locale,
-            "vnp_OrderInfo" => $vnp_OrderInfo,
-            "vnp_OrderType" => $vnp_OrderType,
+            "vnp_IpAddr" => $_SERVER['REMOTE_ADDR'],
+            // "vnp_Locale" => $language,
+            "vnp_Locale"    => "vn",
+            "vnp_OrderInfo" => $orderInfo,
+            "vnp_OrderType" => "billpayment",
             "vnp_ReturnUrl" => $vnp_Returnurl,
-            "vnp_TxnRef" => $vnp_TxnRef,
+            "vnp_TxnRef" => $orderId
         );
 
-        if (isset($bankCode) && $bankCode != "") {
-            $inputData['vnp_BankCode'] = $bankCode;
-        }
-
         ksort($inputData);
+
         $query = "";
-        $i = 0;
         $hashdata = "";
         foreach ($inputData as $key => $value) {
-            if ($i == 1) {
-                $hashdata .= '&' . urlencode($key) . "=" . urlencode($value);
-            } else {
-                $hashdata .= urlencode($key) . "=" . urlencode($value);
-                $i = 1;
-            }
+            $hashdata .= urlencode($key) . "=" . urlencode($value) . '&';
             $query .= urlencode($key) . "=" . urlencode($value) . '&';
         }
 
-        $vnp_Url = $vnp_Url . "?" . $query;
-        if (isset($vnp_HashSecret)) {
-            $vnpSecureHash = hash_hmac('sha512', $hashdata, $vnp_HashSecret);
-            $vnp_Url .= 'vnp_SecureHash=' . $vnpSecureHash;
-        }
+        $hashdata = rtrim($hashdata, '&');
+        $query = rtrim($query, '&');
 
-        return $vnp_Url;
+        $vnpSecureHash = hash_hmac('sha512', $hashdata, $vnp_HashSecret);
+
+        return $vnp_Url . "?" . $query . "&vnp_SecureHash=" . $vnpSecureHash;
     }
+
+
 
     public static function verifyPaymentReturn($getData)
     {
