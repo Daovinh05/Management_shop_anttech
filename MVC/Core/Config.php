da6d6f384f5d6c84da5a58755acb6143cba3f978
phucnad9121 <dunghiep6b@gmail.com>
2026-03-22 00:11:42 +0700

    checkloi

--- MVC/Core/Config.php
+++ MVC/Core/Config.php
@@ -13,7 +13,8 @@ function getBaseUrl()
     }
 
     $protocol = (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off'
-                 || (isset($_SERVER['SERVER_PORT']) && $_SERVER['SERVER_PORT'] == 443))
+                 || (isset($_SERVER['SERVER_PORT']) && $_SERVER['SERVER_PORT'] == 443)
+                 || (isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] == 'https'))
                 ? "https://"
                 : "http://";
 
